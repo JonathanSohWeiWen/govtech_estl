5562b6c8ec395e27360142d4e92297ff8083efdd
Jonathan Soh <e0177194@u.nus.edu>
2020-10-12 21:06:02 +0800

    api loaded table

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const multer = require('multer');
 const bodyParser = require('body-parser');
 const csv = require('fast-csv');
 const fs = require('fs');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'govtech',
+});
 
-// const ENV = process.env.NODE_ENV;
+connection.connect();
 const PORT = process.env.PORT || 5527;
 
 const app = express();
@@ -16,81 +23,114 @@ const upload = multer({ dest: 'uploads/' });
 
 const validateCsvData = (rows) => {
 	// console.log(rows);
+	if (rows.length === 0) return 'Empty csv file';
 	let ids = new Set();
 	let logins = new Set();
-	for (let i = 0; i < rows.length; i++) {
-		if (rows[i].id.charAt(0) === '#') {
+	for (let i = 1; i < rows.length; i++) {
+		if (rows[i].length !== 4) return 'Incorrect number of columns';
+		if (rows[i][0].charAt(0) === '#') {
 			rows.splice(i, 1);
 		} else {
-			ids.add(rows[i].id);
-			logins.add(rows[i].login);
-			if (rows[i].salary < 0) {
+			ids.add(rows[i][0]);
+			logins.add(rows[i][1]);
+			if (rows[i][3] < 0) {
 				return 'Invalid salary';
+			} else if (parseFloat(rows[i][3]).toFixed(2) !== rows[i][3]) {
+				return 'Invalid salary format';
 			}
 		}
 	}
-	if (rows.length - 1 >= ids.size) {
+	if (rows.length - 1 !== ids.size) {
 		return 'Duplicate ids found';
-	} else if (rows.length - 1 >= logins.size) {
+	} else if (rows.length - 1 !== logins.size) {
 		return 'Duplicate login found';
 	}
 	return;
 };
 
-const updateCsvData = (newRows) => {
-	try {
-		let updatedRows = fs.readFileSync('users.json', {
-			encoding: 'utf8',
-			flag: 'r',
-		});
-		let ids = [];
-		for (let i = 0; i < updatedRows.length; i++) {
-			ids.push(updatedRows[i].id);
-		}
-		for (let i = 0; i < newRows.length; i++) {
-			const index = ids.indexOf(newRows[i].id);
-			if (index > 0) {
-				updatedRows[index].login = newRows[i].login;
-				updatedRows[index].name = newRows[i].name;
-				updatedRows[index].salary = newRows[i].salary;
-			} else {
-				updatedRows.push(newRows[i]);
-			}
-		}
-		return updatedRows;
-	} catch (err) {
-		return newRows;
-	}
-};
+app.get('/users', (req, res) => {
+	console.log(req.query);
+	let minSalary = parseInt(req.query.minSalary);
+	if (!isNaN(minSalary)) {
+		if (minSalary < 0)
+			return res.status(400).json({ error: 'Invalid minimum salary' });
+	} else return res.status(400).json({ error: 'Missing minimum salary' });
+	let maxSalary = parseInt(req.query.maxSalary);
+	if (!isNaN(maxSalary)) {
+		if (maxSalary < 0 || maxSalary < minSalary)
+			return res.status(400).json({ error: 'Invalid maximum salary' });
+	} else return res.status(400).json({ error: 'Missing maximum salary' });
+	let limit = parseInt(req.query.limit);
+	if (!isNaN(limit)) {
+		if (limit !== 30)
+			return res.status(400).json({ error: 'Limit must be 30' });
+	} else return res.status(400).json({ error: 'Missing limit' });
+	let offset = parseInt(req.query.offset);
+	if (!isNaN(offset)) {
+		if (offset >= 30)
+			return res.status(400).json({ error: 'Offset must less than 30' });
+	} else return res.status(400).json({ error: 'Missing offset' });
+	let sortParam = req.query.sort;
+	let order = 'ASC';
+	if (sortParam) {
+		if (sortParam.charAt(0) === '-') order = 'DESC';
+		sortParam = sortParam.substring(1);
+		if (
+			sortParam !== 'id' &&
+			sortParam !== 'login' &&
+			sortParam !== 'name' &&
+			sortParam !== 'salary'
+		)
+			return res.status(400).json({ error: 'Invalid sort' });
+	} else return res.status(400).json({ error: 'Missing sort' });
 
-//API calls
-app.get('/', (req, res) => {
-	res.send('Hello world');
+	const sqlQuery =
+		'SELECT * FROM user WHERE salary>=' +
+		minSalary +
+		' AND salary<=' +
+		maxSalary +
+		' ORDER BY ' +
+		sortParam +
+		' ' +
+		order +
+		' LIMIT ' +
+		limit +
+		' OFFSET ' +
+		offset;
+	connection.query(sqlQuery, (err, result) => {
+		if (err) throw err;
+		res.header('Access-Control-Allow-Origin', '*');
+		return res.send(result);
+	});
 });
+
 app.post('/users/upload', upload.single('file'), (req, res) => {
 	const fileRows = [];
 	csv
 		.parseFile(req.file.path, 'utf-8')
 		.on('data', (data) => {
-			fileRows.push({
-				id: data[0],
-				login: data[1],
-				name: data[2],
-				salary: data[3],
-			});
+			fileRows.push(data);
 		})
 		.on('end', () => {
-			fileRows.splice(0, 1);
 			fs.unlinkSync(req.file.path);
 			const validationError = validateCsvData(fileRows);
 			if (validationError) {
 				return res.status(400).json({ error: validationError });
 			} else {
-				const updatedRows = updateCsvData(fileRows);
-				const writeData = JSON.stringify(updatedRows);
-				fs.writeFile('users.json', writeData, (err) => {
-					if (err) throw err;
+				fileRows.splice(0, 1);
+				fileRows.forEach((row) => {
+					row[3] = parseFloat(row[3]);
 				});
+				connection.query(
+					'REPLACE INTO user (id, login, name, salary) VALUES ?',
+					[fileRows],
+					(err, result) => {
+						if (err) throw err;
+						console.log(
+							'Number of records inserted/updated: ' + result.affectedRows
+						);
+					}
+				);
 				return res.json({ message: 'Upload successful' });
 			}
 		});
--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+	const [params, setParams] = useState({
+		minSalary: 0,
+		maxSalary: 4000,
+		offset: 0,
+		limit: 30,
+		sort: '+name',
+	});
+
+	useEffect(() => {
+		axios
+			.get('http://localhost:5527/users/', {
+				params: params,
+			})
+			.then((res) => {
+				setUsers(res.data);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [params]);
+
+	return (
+		<div>
+			<table class='table-auto'>
+				<thead>
+					<tr>
+						<th class='px-4 py-2'>ID</th>
+						<th class='px-4 py-2'>Login</th>
+						<th class='px-4 py-2'>Name</th>
+						<th class='px-4 py-2'>Salary</th>
+					</tr>
+				</thead>
+				<tbody>
+					{users.map((item, index) => {
+						if (index % 2 === 0) {
+							return (
+								<tr key={item.id}>
+									<td class='border px-4 py-2'>{item.id}</td>
+									<td class='border px-4 py-2'>{item.login}</td>
+									<td class='border px-4 py-2'>{item.name}</td>
+									<td class='border px-4 py-2'>{item.salary}</td>
+								</tr>
+							);
+						} else {
+							return (
+								<tr class='bg-gray-100' key={item.id}>
+									<td class='border px-4 py-2'>{item.id}</td>
+									<td class='border px-4 py-2'>{item.login}</td>
+									<td class='border px-4 py-2'>{item.name}</td>
+									<td class='border px-4 py-2'>{item.salary}</td>
+								</tr>
+							);
+						}
+					})}
+				</tbody>
+			</table>
+		</div>
+	);
 }
 
 export default App;
